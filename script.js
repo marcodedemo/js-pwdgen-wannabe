4c8e54245cb38c1d38a5a02525597bd8734159ef
marcodedemo <marco.dedemo@gmail.com>
2023-02-21 13:01:22 +0100

    feat: script

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+
+let nomeUtente = prompt("Come ti chiami?");
+
+let cognomeUtente = prompt("Qual'è il tuo cognome? inseriscilo senza spazi");
+
+let colorePreferito = prompt("Qual'è il tuo colore preferito?");
+
+const numeroCheDecidoIo = 27;
+
+document.writeln(nomeUtente + cognomeUtente + colorePreferito + numeroCheDecidoIo)
